f9b046959dcf9bf527660024e0967ac8b84936eb
TOM261105 <tomas.gtz.vsco@gmail.com>
2026-02-03 13:39:41 -0600

    Update products.js

--- products.js
+++ products.js
@@ -13,11 +13,11 @@ const products = [
     name: "Salsa Roja",
     price: 200,
     stripeLink: "https://buy.stripe.com/TU_LINK_3"
-  } 
+  },
   {
     name: "Salsa 3",
     price: 200,
-    stripeLink: "https://buy.stripe.com/TU_LINK_1"
+    stripeLink: "https://buy.stripe.com/TU_LINK_4"
   }
 ];
 
@@ -28,7 +28,7 @@ products.forEach(product => {
     <div class="product">
       <h2>${product.name}</h2>
       <p>Precio: $${product.price} MXN</p>
-      <a href="${product.stripeLink}">
+      <a href="${product.stripeLink}" target="_blank">
         <button>Comprar</button>
       </a>
     </div>
